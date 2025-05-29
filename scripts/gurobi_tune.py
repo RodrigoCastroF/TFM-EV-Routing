ca4a97685116fa64f990154a3cc5135853b4a88e
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2025-05-29 18:22:26 +0200

    Run tuning script

--- scripts/gurobi_tune.py
+++ scripts/gurobi_tune.py
@@ -3,15 +3,19 @@ import gurobipy as gp
 
 if __name__ == "__main__":
 
+    model_path = "../data/37-intersection map EV1 Model.mps"
+    parameters_prefix = "../data/37-intersection map EV1 Tuned Parameters"
+    hours_tuning = 2
+
     params = {
-        'TuneOutput': 3,
+        'TuneOutput': 2,
         'TuneTargetMIPGap': 0.05,
-        'TuneTargetTime': 30,
-        'TuneTimeLimit': 300,
+        'TuneTargetTime': 10,  # Try to reach target gap in less than this
+        'TuneTimeLimit': 3600 * hours_tuning,
         'TuneTrials': 1,
+        'TimeLimit': 60,  # Never solve for more than this
     }
 
-    model_path = "../data/37-intersection map EV1 Model.mps"
     model = gp.read(model_path)
     for param, val in params.items():
         model.setParam(param, val)
@@ -26,10 +30,10 @@ if __name__ == "__main__":
     if tune_result_count > 0:
         for i in range(tune_result_count):
             model.getTuneResult(i)
-            result_file = f"../data/tuned_params_{i}.prm"
+            result_file = f"{parameters_prefix} {i}.prm"
             model.write(result_file)
             print(f"Saved result {i} to {result_file}")
-        print(f"Best parameters saved as tuned_params_0.prm")
+        print(f"Best parameters saved as {parameters_prefix} 0.prm")
     else:
         print("No improvements found.")
 
