217d68a16f163548968f8852848c1e4159fe42ed
Rodrigo Castro Freibott <79080278+RodrigoCastroF@users.noreply.github.com>
2025-06-07 18:02:43 +0200

    Cloned optiCL

---
+++ opticl/__init__.py
@@ -0,0 +1,7 @@
+"""
+This is a copy of https://github.com/hwiberg/OptiCL
+The original code was modified so it can work with newer versions of sklearn and pandas
+"""
+from .constraint_learning import *
+from .embed_mip import *
+from .run_MLmodels import *
